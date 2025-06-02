fa9421e0bf66329160a28e7b5d70fc8fd869287c
gnohnoey <mj9514884@gmail.com>
2025-06-02 11:07:06 +0900

    compound operator

--- src/Operator.java
+++ src/Operator.java
@@ -3,19 +3,16 @@ public class Operator {
         int a = 10;
         int b = 3;
 
-        int sum = a + b;
-        System.out.println(sum);
+        a+=5;
+        System.out.println(a);
 
-        int difference = a-b;
-        System.out.println(difference);
-
-        int product = a*b;
-        System.out.println(product);
-
-        int quotient = a/b;
-        System.out.println(quotient);
-
-        int remainder = a%b;
-        System.out.println(remainder);
+        b-=1;
+        System.out.println(b);
+        b*=2;
+        System.out.println(b);
+        b/=3;
+        System.out.println(b);
+        b%=4;
+        System.out.println(b);
     }
 }
