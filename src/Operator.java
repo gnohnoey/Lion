150e56f5d9d926357cb27244417e335cbd2da288
gnohnoey <mj9514884@gmail.com>
2025-06-02 10:36:03 +0900

    operator

---
+++ src/Operator.java
@@ -0,0 +1,21 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+        int difference = a-b;
+        System.out.println(difference);
+
+        int product = a*b;
+        System.out.println(product);
+
+        int quotient = a/b;
+        System.out.println(quotient);
+
+        int remainder = a%b;
+        System.out.println(remainder);
+    }
+}
