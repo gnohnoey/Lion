53bcb50239fac0b57cad0876a0a7a1a1b9d84658
gnohnoey <mj9514884@gmail.com>
2025-06-02 09:40:55 +0900

    first commit

---
+++ src/Const.java
@@ -0,0 +1,9 @@
+public class Const {
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println(a);
+
+        //a=11;
+        System.out.println(a);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println(age);
+
+        age = 26;
+        System.out.println(age);
+
+        double salary = 50000.75;
+        System.out.println(salary);
+
+        String name = "조연홍";
+        System.out.println(name);
+    }
+}
